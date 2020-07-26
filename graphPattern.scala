31d3268e80290eba731fe405ecb308e754ab2cf9
dyb392 <50106477+dyb392@users.noreply.github.com>
2020-07-26 21:42:18 +0800

    Add files via upload

--- graphPattern.scala
+++ graphPattern.scala
@@ -1,3 +1,8 @@
+import org.apache.spark._
+import org.apache.spark.graphx._
+// To make some of the examples work we will also need RDD
+import org.apache.spark.rdd.RDD
+
 
 // Assume the SparkContext has already been constructed
 val sc: SparkContext
@@ -14,6 +19,11 @@ val defaultUser = ("John Doe", "Missing")
 // Build the initial Graph
 val graph = Graph(users, relationships, defaultUser)
 
+
+import org.apache.spark._
+import org.apache.spark.graphx._
+// To make some of the examples work we will also need RDD
+import org.apache.spark.rdd.RDD
 val studentsPattern: RDD[(VertexId, String)] = 
     sc.parallelize(Array((1L, "s"), (2L, "u"), (3L, "d"), (4L, "v"),
                         (5L, "w"), (6L, "c")))
@@ -21,14 +31,14 @@ val studentsPattern: RDD[(VertexId, String)] =
 val timesPattern: RDD[Edge[(Int, Int)]] = 
     sc.parallelize(Array(Edge(1L, 2L, (10900, 11000)), Edge(2L, 3L, (11400, 11500)),
                         Edge(1L, 4L, (10700, 10800)), Edge(2L, 4L, (40800, 40900)),
-                        Edge(2L, 5L, (21500, 21600), Edge(2L, 6L, (21000, 21100)))))
+                        Edge(2L, 5L, (21500, 21600)), Edge(2L, 6L, (21000, 21100))))
 
 val patternGraph = Graph(studentsPattern, timesPattern)
 
-val studentsData: RDD[(VertexId, (String, String)))] = 
+val studentsData: RDD[(VertexId, (String, String))] = 
     sc.parallelize(Array((1L, ("Dan", "s")), (2L, ("Jane", "u")), (3L, ("Sclina", "d")), (4L, ("Pat", "v")),
                         (5L, ("Ross", "w")), (6L, ("Jenny", "c")), (7L, ("Gray", "s")), (8L, ("Mark", "u")),
-                        (9L, ("Gaia", "d")), (10L, ("Jessica", "v"), (11L, ("Bill", "w")))))
+                        (9L, ("Gaia", "d")), (10L, ("Jessica", "v")), (11L, ("Bill", "w"))))
 
 val timesData: RDD[Edge[(Int, Int)]] = 
     sc.parallelize(Array(Edge(1L, 2L, (10900, 10940)), Edge(2L, 3L, (11400, 11420)),
@@ -41,4 +51,8 @@ val timesData: RDD[Edge[(Int, Int)]] =
                         Edge(10L, 6L, (11600, 11700)), Edge(10L, 9L, (21500, 21600))))
 
 val dataGraph = Graph(studentsData, timesData)
+
+dataGraph.vertices.filter { case (id, (name, level)) => id > 0}.collect.foreach {
+  case(id, (name, level)) => println(s"$name's level is $level")
+}
 
